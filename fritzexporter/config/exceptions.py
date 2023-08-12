baafe01d6a8fffbe241db03b6fa813f4248ddb8d
Patrick Dreker <patrick@dreker.de>
2023-08-12 08:49:40 +0200

    docs: update copyright boilerplate

--- fritzexporter/config/exceptions.py
+++ fritzexporter/config/exceptions.py
@@ -26,7 +26,7 @@ class FritzPasswordTooLongError(ExporterException):
     pass
 
 
-# Copyright 2019-2022 Patrick Dreker <patrick@dreker.de>
+# Copyright 2019-2023 Patrick Dreker <patrick@dreker.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
